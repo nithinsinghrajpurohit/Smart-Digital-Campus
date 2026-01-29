40bdcd949bc975b94311434f0583cf5c762824f1
nithinsinghrajpurohit <nithinsinghrajpurohit@gmail.com>
2026-01-29 23:21:16 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 // Use environment variable for the API base URL.
 // Fallback to localhost for local development.
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://smart-digital-campus-1.onrender.com";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://smart-campus-drwa.onrender.com";
 
 export default API_BASE_URL;
+
